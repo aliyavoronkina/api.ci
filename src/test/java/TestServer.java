3567e8ebe0daaacf97e3e28f6b1af1833276954e
Aliya Voronkina <atleugabilova@mail.ru>
2025-10-07 18:11:54 +0500

    Add SUT jar file and update CI config

--- src/test/java/TestServer.java
+++ src/test/java/TestServer.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 
 public class TestServer {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(9999), 0);
+        server.createContext("/api/accounts", new AccountsHandler());
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Test Server started on port 9999");
+    }
+
+    static class AccountsHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String response = "[" +
+                    "{" +
+                    "\"id\": 1," +
+                    "\"name\": \"Сберегательный счет\"," +
+                    "\"number\": \"** 0001\"," +
+                    "\"balance\": 100000," +
+                    "\"currency\": \"RUB\"" +
+                    "}," +
+                    "{" +
+                    "\"id\": 2," +
+                    "\"name\": \"Текущий счет\"," +
+                    "\"number\": \"** 0002\"," +
+                    "\"balance\": 50000," +
+                    "\"currency\": \"USD\"" +
+                    "}" +
+                    "]";
+            exchange.getResponseHeaders().set("Content-Type", "application/json");
+            exchange.sendResponseHeaders(200, response.getBytes().length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
+    }
 }
